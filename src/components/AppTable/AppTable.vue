1f54c65cb79286a447ac88fb10b1a086f9badc7f
dev-dtn1712 <dev.danguyen1712@gmail.com>
2023-04-10 20:19:32 -0700

    update table components

--- src/components/AppTable/AppTable.vue
+++ src/components/AppTable/AppTable.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
     <v-text-field v-model="search" label="Search" dense outlined />
-      <v-data-table
-        :headers="headers"
-        :items="filteredItems"
-        :sort-by.sync="sortBy"
-        :sort-desc.sync="sortDesc"
-        :loading="loading"
-        class="custom-header"
-      >
+    <v-data-table
+      :headers="headers"
+      :items="items"
+      :search="search"
+      :sort-by.sync="sortBy"
+      :sort-desc.sync="sortDesc"
+      :loading="loading"
+      class="custom-header"
+    >
       <template v-slot:item.total_donations="{ item }">
         {{ formatCurrency(item.total_donations) }}
       </template>
@@ -32,20 +33,6 @@
         sortBy: 'full_name',
         sortDesc: false,
       }
-    },
-    computed: {
-      filteredItems() {
-        return this.items.filter((donor) =>
-          Object.values(donor).some(
-            (fieldValue) =>
-              fieldValue &&
-              fieldValue
-                .toString()
-                .toLowerCase()
-                .includes(this.search.toLowerCase())
-          )
-        );
-      },
     },
     methods: {
       formatCurrency(amount) {
